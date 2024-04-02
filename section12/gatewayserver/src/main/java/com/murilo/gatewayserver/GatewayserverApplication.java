29b23cba92af4ffb849b24fde26d6ecd5c5652a1
Murilo Vieira <murilo12super@gmail.com>
2024-04-02 16:57:58 -0300

    Atualizado eco sistema docker para aceitar requisições apenas do gateway

--- section12/gatewayserver/src/main/java/com/murilo/gatewayserver/GatewayserverApplication.java
+++ section12/gatewayserver/src/main/java/com/murilo/gatewayserver/GatewayserverApplication.java
@@ -2,11 +2,6 @@ package com.murilo.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
-
-import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class GatewayserverApplication {
