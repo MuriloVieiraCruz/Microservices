ae843e10320f0681ec6cd66ac5e895e4e223c8a4
Murilo Vieira <murilo12super@gmail.com>
2024-04-08 16:23:28 -0300

    Iniciado implementação de mensageria no sistema

---
+++ section13/message/src/test/java/com/eazybytes/message/MessageApplicationTests.java
@@ -0,0 +1,9 @@
+package com.eazybytes.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageApplicationTests {
+
+}
